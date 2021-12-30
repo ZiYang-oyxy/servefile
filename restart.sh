5e5723eabb1273343ace522b5ef0cea8c3e949ce
ziyang <hh123okbb@gmail.com>
2021-12-30 14:38:34 +0800

    fix "页面可能存在问题，或者已永久移动到新的网址"

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ db_dir=~/.ad_database
 
 killall servefile
 mkdir -p $db_dir
-nohup servefile -u -a Awesome:Devops --realm ad -p 8889 -4 $db_dir >/dev/null 2>&1 &
+nohup servefile -u -a Awesome:Devops -p 8889 -4 $db_dir >/dev/null 2>&1 &
 sleep 1
-nohup servefile -l -a Awesome:Devops --realm ad -p 8890 -4 $db_dir >/dev/null 2>&1 &
+nohup servefile -l -a Awesome:Devops -p 8890 -4 $db_dir >/dev/null 2>&1 &
 sleep 2
